6fef2194384d4481dd9403acf1f695c12742f1c0
Blat gfServer process <blat@blat4trackhubs2.novalocal>
2016-05-20 20:02:45 +0000

    automatically get blat exe, if missing

--- blat_servers_start.sh
+++ blat_servers_start.sh
@@ -1,12 +1,26 @@
+SERVER_EXE=ucsc_kent/2016-05-10/blat/gfServer
+HUB_URL=https://data.genome.uab.edu/public/ucsc_track_hubs/hcmv_pub
 # 
 # mirror .2bit files from data.genome.uab.edu
 #
-wget -m https://data.genome.uab.edu/public/ucsc_track_hubs/hcmv_pub/
+wget -m $HUB_URL
+
+#
+# get blat exe 
+#
+if [ ! -e ucsc_kent/2016-05-10/blat/gfServer ]; then
+	(cd ucsc_kent/2016-05-10 && ./pull.sh)
+fi
+
+#
+# dir for output logs
+#
+mkdir -p logs
 
 #
 # per https://genome.ucsc.edu/goldenpath/help/hubQuickStartAssembly.html#blat
 # start 2 servers - one for translated, one for straight up
-mkdir -p logs
+#
 
 # hcmv only for now
 GENOMES=$(find data.genome.uab.edu/public/ucsc_track_hubs/hcmv_pub -name "*.2bit")
