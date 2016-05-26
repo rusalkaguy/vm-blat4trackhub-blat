602d35c0232557b61c28f67a74d813482bcd69ef
Curtis <curtish@uab.edu>
2016-05-26 19:09:14 +0000

    switch to one genome per gfServer, per hub architectures. If have multiple genomes, need mulitiple gfServer pairs! Next: drive creation of servers (including ports) from genomes.txt

--- blat_servers_start.sh
+++ blat_servers_start.sh
@@ -3,7 +3,12 @@ HUB_URL=https://data.genome.uab.edu/public/ucsc_track_hubs/hcmv_pub
 # 
 # mirror .2bit files from data.genome.uab.edu
 #
-wget -m $HUB_URL
+# could be improved with something like
+#wget -r --no-parent --reject "index.html*" -nH --cut-dirs=3 http://genome.ucsc.edu/goldenPath/help/examples/hubExamples/hubAssembly/plantAraTha1/
+# ie wget -q -r --no-parent --reject "index.html*" -nH --cut-dirs=2 https://data.genome.uab.edu/public/ucsc_track_hubs/hcmv_pub/
+echo "Pulling genomes from $HUB_URL"
+#wget -m $HUB_URL
+wget -q -r --no-parent --reject "index.html*" -nH --cut-dirs=2 $HUB_URL/
 
 #
 # get blat exe 
@@ -23,7 +28,16 @@ mkdir -p logs
 #
 
 # hcmv only for now
-GENOMES=$(find data.genome.uab.edu/public/ucsc_track_hubs/hcmv_pub -name "*.2bit")
-nohup ucsc_kent/2016-05-10/blat/gfServer start localhost 17777 -trans -mask ${GENOMES} > logs/gfServer.17777.log &
-nohup ucsc_kent/2016-05-10/blat/gfServer start localhost 17779 -stepSize=5  ${GENOMES} > logs/gfServer.17779.log &
+#GENOMES=$(find data.genome.uab.edu/public/ucsc_track_hubs/hcmv_pub -name "*.2bit")
+#for x in $GENOMES; do ln -fs $x . ; done
+#GENOMES_2BIT=$(ls *.2bit)
+ROOTDIR=$PWD
+cd hcmv_pub/hh5Merlin2
+GENOME=$(basename $PWD)
+echo GENOME=$GENOME
+GENOME_2BIT=$(find . -name "*.2bit")
+echo GENOME_2BIT=$GENOME_2BIT
+nohup $ROOTDIR/ucsc_kent/2016-05-10/blat/gfServer start localhost 17777 -trans       -seqLog -ipLog -log=$ROOTDIR/logs/$GENOME.pro.17777.log ${GENOME_2BIT} &
+nohup $ROOTDIR/ucsc_kent/2016-05-10/blat/gfServer start localhost 17779 -stepSize=5  -seqLog -ipLog -log=$ROOTDIR/logs/$GENOME.dna.17779.log ${GENOME_2BIT} &
+
 
--- blat_servers_stop.sh
+++ blat_servers_stop.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-ucsc_kent/2016-05-10/blat/gfServer stop localhost 17777
-ucsc_kent/2016-05-10/blat/gfServer stop localhost 17779
+
+# these don't work
+#ucsc_kent/2016-05-10/blat/gfServer stop localhost 17777
+#ucsc_kent/2016-05-10/blat/gfServer stop localhost 17779
+
+# brute force
+ps -eaf | grep gf | awk '{print $2}' | xargs kill -9
+
