36eab7bccfdc12230c2f3cd1f9df54e89259b70c
CCTS Bioinformatics group at UAB <bioinfo@vo.uabgrid.uab.edu>
2020-02-27 16:26:37 -0600

    use mktemp for test.sh output file, so can run as any user

--- test.sh
+++ test.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 # test if our blat server is working
+echo "========== mktemp ============"
+outPsl=$(mktemp --suffix=.psl)
+echo "# mktemp=$outPsl"
+
 echo "========== gcClient localhost 17779 ============"
-./ucsc_kent/2016-05-10/blat/gfClient -t=dna -q=dna -minScore=5 localhost 17779 hcmv_pub/hh5Merlin2 test.fa out.psl
-echo "========== out.psl ============"
-cat out.psl
+./ucsc_kent/2016-05-10/blat/gfClient -t=dna -q=dna -minScore=5 localhost 17779 hcmv_pub/hh5Merlin2 test.fa $outPsl
+echo "========== $outPsl ============"
+cat $outPsl
 echo "========== diff out.psl test.psl ========="
-diff -s out.psl test.psl
+diff -s $outPsl test.psl
+
+rm $outPsl
 
